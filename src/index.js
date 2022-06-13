3e53b275a1ba2b5a0b86a4c6be6aa83e62a6b794
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-06-13 18:52:29 -0300

    Inital Commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import "dotenv/config"
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get("/", async (req, res) => {
+	res.send("Hello World!")
+})
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log(`Server is running on port ${process.env.PORT || 5000} 🐱‍👤`)
+})
