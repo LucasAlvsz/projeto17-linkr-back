1926648a026a66d48e15914d9c99732dcb53e233
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-06-14 19:37:13 -0300

    feat: single middleware for all body schemas implemented

---
+++ src/middlewares/schemaValidateMiddleware.js
@@ -0,0 +1,10 @@
+export const schemaValidateMiddleware = (schema) => {
+    return (schemaValidateMiddleware[schema] = (req, res, next) => {
+        const { error } = schema.validate(req.body, { abortEarly: false });
+        if (error)
+            return res
+                .status(422)
+                .send(error.details.map(({ message }) => message));
+        next();
+    });
+};
