5187516db33fb24e46163a81103df865e2eae802
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-06-13 21:48:13 -0300

    feat: database connection implemented

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import pg from "pg"
+
+const { Pool } = pg
+
+const db = new Pool({
+	connectionString: process.env.DATABASE_URL,
+	...(process.env.PROD_MODE && {
+		ssl: { rejectUnauthorized: false },
+	}),
+})
+
+export default db
