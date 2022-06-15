787f51f11a0e1875e09c0f8d64cf36bc38d16492
Lucas A <contato.lucasalv@gmail.com>
2022-06-15 00:30:37 -0300

    space between imports

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 import hashtagsRoutes from "./hashtagsRoutes.js";
 import userPost from "./userPostRoutes.js";
+
 const router = Router();
 
 router.use(hashtagsRoutes);
