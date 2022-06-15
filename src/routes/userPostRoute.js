f0cf96f0453df767eb2c507095c249883b755b81
Natan Brito <natanismaelbrito@gmail.com>
2022-06-14 23:40:56 -0300

    CHORE starting building endpoint /post make for user

--- src/routes/userPostRoute.js
+++ src/routes/userPostRoute.js
@@ -1,6 +1,8 @@
-import {Router } from "express";
-import {postUser} from "../controllers/userPostController.js";
-const userPost= Router();
-userPost.post("/post", postUser);
+import { Router } from "express";
+import { postUser } from "../controllers/userPostController.js";
+import schemaValidateMiddleware from "../middlewares/schemaValidateMiddleware.js";
+import postSchema from "../schemas/userPostSchema.js";
+const userPost = Router();
+userPost.post("/post", schemaValidateMiddleware(postSchema), postUser);
 
 export default userPost;
