586508dee54af0d5f73864db8a0b3888e2ac6d54
Thales Gomes Targino <thalestargino@gmail.com>
2022-06-17 08:56:09 -0300

    fix: userPageController and response user posts with metadata

--- src/controllers/userPageController.js
+++ src/controllers/userPageController.js
@@ -1,26 +1,24 @@
 import verboseLog from "../utils/verboseLog.js";
 import userPageRepository from "../repositories/userPageRepository.js";
-import urlMetadata from "url-metadata";
+import joinUrlMetadataWithPostData from "../services/joinUrlMetadataWithPostData.js";
 
 export const getUserPageData = async (req, res) => {
     const { id } = req.params;
     try {
         const user = res.locals.user;
-        const posts = await userPageRepository.getUserPostsById(id);
-        const postsMetadata = [];
-        const getMetadataUrl = async () => {
-            for (const post of posts) {
-                const metadata = await urlMetadata(post.link);
-                postsMetadata.push({
-                    postTitle: metadata.title,
-                    postUrl: metadata.url,
-                    postDescription: metadata.description,
-                    postImage: metadata.image,
-                });
-            }
-        };
-        await getMetadataUrl();
-        res.send({ user: user.name, posts: postsMetadata });
+        const result = await userPageRepository.getUserPostsById(id);
+        const formattedPosts = await joinUrlMetadataWithPostData(result.rows);
+
+        res.send({
+            user: [
+                {
+                    username: user.username,
+                    userid: user.id,
+                    userpic: user.userpic,
+                },
+            ],
+            posts: formattedPosts,
+        });
     } catch (err) {
         verboseLog(err);
         res.sendStatus(500);
