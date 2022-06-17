2e8c1db061d840f3af938b40db00503f8edf50f4
Thales Gomes Targino <thalestargino@gmail.com>
2022-06-17 11:31:08 -0300

    feat: search users for searchBar with query string

--- src/repositories/userPageRepository.js
+++ src/repositories/userPageRepository.js
@@ -1,35 +1,43 @@
 import db from "../db/index.js";
 
 const getUserPostsById = async (userId) => {
-    return await db.query(
+    const { rows } = await db.query(
         `--sql
-    SELECT posts.id,
-    users.id AS "userId", 
-    posts.link,
-    posts.article,
-    posts."createdAt",
-    users.username, 
-    users."pictureUrl" AS "userpic"
+    SELECT posts.link, 
+    users.username as "userName"
     FROM posts
     JOIN users ON posts."userId" = users.id
     WHERE users.id = $1
     `,
         [userId],
     );
+    return rows;
 };
 
 const getUserById = async (userId) => {
     const { rows } = await db.query(
         `--sql
-    SELECT users.id, users.username, users."pictureUrl" AS "userpic" FROM users WHERE id = $1`,
+    SELECT users.id, users.username FROM users WHERE id = $1`,
         [userId],
     );
     return rows[0];
 };
 
+const getListOfUsersSearchBar = async (search) => {
+    const { rows } = await db.query(
+        `--sql
+        SELECT users.id, users.username 
+        FROM users
+        WHERE username ILIKE $1`,
+        [`${search}%`],
+    );
+    return rows;
+};
+
 const userPageRepository = {
     getUserPostsById,
     getUserById,
+    getListOfUsersSearchBar,
 };
 
 export default userPageRepository;
--- src/routes/userPageRoutes.js
+++ src/routes/userPageRoutes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getUserPageData } from "../controllers/userPageController.js";
+import {
+    getUserPageData,
+    getUsersSearchBar,
+} from "../controllers/userPageController.js";
 import getUserPageMiddleware from "../middlewares/getUserPageMiddleware.js";
 
 const userPageRouter = Router();
 
 userPageRouter.get("/user/:id", getUserPageMiddleware, getUserPageData);
+userPageRouter.get("/user", getUsersSearchBar);
 
 export default userPageRouter;
