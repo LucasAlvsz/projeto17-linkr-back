2b391e97b2bd1ce8c1c1cbfb28b890eadb2d8226
joaohdallago <joaohdallago@gmail.com>
2022-06-19 19:03:38 -0300

    feat: add token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,13 @@
 import authRepository from "../repositories/authRepository.js";
+import encryptPassword from "../services/encryptPassword.js";
+import JWTGenerator from "../services/JWTGenerator.js";
 import verboseLog from "../utils/verboseLog.js";
-
 
 export const postSignUp = async (req, res) => {
     try {
         const signUpData = req.body;
 
+        signUpData.password = encryptPassword(req.body.password);
         await authRepository.insertNewUser(signUpData);
 
         res.sendStatus(201);
@@ -17,7 +19,11 @@ export const postSignUp = async (req, res) => {
 
 export const postSignIn = (req, res) => {
     try {
-        res.send("teste signin");
+        const { user } = res.locals;
+        const { id, pictureUrl } = user;
+
+        const token = JWTGenerator(id);
+        res.send({token, pictureUrl});
     } catch (error) {
         verboseLog(error);
         res.sendStatus(500);
--- src/repositories/authRepository.js
+++ src/repositories/authRepository.js
@@ -2,20 +2,19 @@ import db from "../db/index.js";
 
 const selectUserByEmail = async (email) => {
     return await db.query(`
-        SELECT email
+        SELECT *
         FROM users
         WHERE email = $1
     `, [email]);
 };
 
 const insertNewUser = async (signUpData) => {
-    //falta o pictureUrl
-    const { email, password, username } = signUpData;
+    const { email, password, username, pictureUrl } = signUpData;
     
     return await db.query(`
-        INSERT INTO users (email, password, name)
-        VALUES ($1, $2, $3)
-    `, [email, password, username]);
+        INSERT INTO users (email, password, username, "pictureUrl")
+        VALUES ($1, $2, $3, $4)
+    `, [email, password, username, pictureUrl]);
 };
 
 const authRepository = {
