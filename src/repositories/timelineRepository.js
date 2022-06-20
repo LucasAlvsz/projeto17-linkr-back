8efa5b10a933b5a211979b189217b24d8d400227
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-06-20 10:54:15 -0300

    fix: fix all repositories to get likes too

--- src/repositories/timelineRepository.js
+++ src/repositories/timelineRepository.js
@@ -3,17 +3,20 @@ import db from "../db/index.js";
 const CatchingPost = async () => {
     return await db.query(`
         --sql
-    SELECT posts.id,
-    users.id AS "userId", 
-    posts.link,
-    posts.article,
-    posts."createdAt",
-    users.username, 
-    users."pictureUrl" AS "userpic"
-    FROM posts
-    JOIN users ON posts."userId" = users.id
-    ORDER BY id DESC
-    LIMIT 20
+        SELECT posts.id,
+        users.id AS "userId", 
+        posts.link,
+        posts.article,
+        posts."createdAt",
+        users.username, 
+        users."pictureUrl" AS "userpic",
+        COUNT(likes.id) AS "likes"
+        FROM posts
+        LEFT JOIN likes ON likes."postId" = posts.id
+        JOIN users ON posts."userId" = users.id
+        GROUP BY posts.id, users.id
+        ORDER BY id DESC
+        LIMIT 20
     `);
 };
 
