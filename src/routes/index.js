4366b0e8a74b163fc0caf032adef6916cf85786c
Gabriel Viana Boaes <viana_gab2001@hotmail.com>
2022-06-20 08:32:34 -0300

    chore: like done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,17 @@
 import { Router } from "express";
 
+import authRouter from "./authRouter.js";
 import hashtagsRoutes from "./hashtagsRoutes.js";
 import userPost from "./userPostRoutes.js";
 import userPageRouter from "./userPageRoutes.js";
+import likeRouter from "./likeRouter.js";
 
 const router = Router();
 
+router.use(authRouter);
 router.use(hashtagsRoutes);
 router.use(userPost);
 router.use(userPageRouter);
+router.use(likeRouter);
 
 export default router;
